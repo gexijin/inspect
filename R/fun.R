b12fd6792625c6739bcc6ebf82e38bbb0b666ccf
Steven Ge <xijin.ge@sdstate.edu>
2024-01-08 21:55:28 -0600

    R package

---
+++ R/fun.R
@@ -0,0 +1,77 @@
+
+#' Prepare User input.
+#'
+#' The response from GPT3 sometimes contains strings that are not R commands.
+#'
+#' @param x A number
+#'
+#' @return Returns a number.
+#' @export
+add3 <- function (x) {
+  x + 3
+}
+
+
+#' Create a report for exploratory data analysis (EDA)
+#'
+#' This function generates a comprehensive EDA report and attempts to open it in a browser.
+#'
+#' @param df A data.frame or a matrix with at least 2 columns.
+#' @param target A selected target variable.
+#' @examples
+#' # Example with default parameters (using iris dataset)
+#' eda_report()
+#'
+#' # Example with custom data frame and target variable
+#' my_data <- data.frame(x = rnorm(100), y = rnorm(100))
+#' eda_report(df = my_data, target = "x")
+#'
+#' @details
+#' The function performs a check on the input data to ensure it is either a data frame or matrix.
+#' It also checks that the input data has at least two columns. The function uses a predefined Rmarkdown template (`eda.Rmd`)
+#' to generate the report. This template should be present in the `inspect` package.
+#'
+#' @seealso
+#' \code{\link[rmarkdown]{render}} for details on the rendering of Rmarkdown documents.
+#' \code{\link[utils]{browseURL}} for how URLs or files are opened in browsers.
+#'
+#' @return This function does not return a value. Instead, it generates an HTML file containing the EDA report and attempts to open this file in the default web browser.
+#' @export
+eda_report <- function(df = iris, target = "Species") {
+  # Validate input
+  if(is.null(df)) {
+    stop("No data frame provided.")
+  }
+  if(!is.data.frame(df) && !is.matrix(df)) {
+    stop("The provided data is neither a data frame nor a matrix.")
+  }
+  if(ncol(df) < 2) {
+    stop("The data frame/matrix should have at least two columns.")
+  }
+
+  # Define file paths
+  rmd_file <- "inspect_EDA.Rmd"
+  output_file <- "inspect_EDA_report.html"
+
+  # Copy the Rmd file template
+  file.copy(from = system.file("eda.Rmd", package = "inspect"), to = rmd_file, overwrite = TRUE)
+
+  # Set up parameters to pass to the Rmd document
+  params <- list(df = df, target = target)
+
+  # Render the Rmarkdown document
+  tryCatch({
+    rmarkdown::render(input = rmd_file, output_file = output_file, params = params, envir = new.env(parent = globalenv()))
+    # Attempt to open the HTML report in a browser
+    utils::browseURL(output_file)
+  }, error = function(e) {
+    cat("Error when generating the report: ", e$message, "\n")
+  }, finally = {
+    cat("Report generation process completed.\n")
+  })
+}
+
+
+
+
+
